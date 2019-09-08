edefb2b86152a51f32dc3dcda8a64417379ac353
lunify <51797974+lunify@users.noreply.github.com>
2019-09-09 00:22:38 +0100

    Integrate prettier with eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     es6: true,
     node: true
   },
-  extends: 'eslint:recommended',
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly'
