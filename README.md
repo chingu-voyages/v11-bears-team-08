28fad466074fc78e7cd9f1fb278a4d5f2a05b1d7
lunify <51797974+lunify@users.noreply.github.com>
2019-09-09 01:18:20 +0100

    Update codebase to conform to the project's styleguide

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # v11-bears-team-08
+
 Add-project-description-here | Voyage-11 | https://chingu.io/ | Twitter: https://twitter.com/ChinguCollabs
--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-const express = require('express');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
+const express = require('express')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
 
 // load dev env variables using dotenv
 if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config({ path: '.env.development.local' });
+  require('dotenv').config({ path: '.env.development.local' })
 }
 
-const app = express();
+const app = express()
 
-app.use(express.json(), express.urlencoded({ extended: true }));
+app.use(express.json(), express.urlencoded({ extended: true }))
 if (process.env.NODE_ENV === 'production') {
-  app.use(morgan('combined'));
+  app.use(morgan('combined'))
 } else {
-  app.use(morgan('dev'));
+  app.use(morgan('dev'))
 }
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.info(`Successfully connected to port ${PORT}`);
-});
+  console.info(`Successfully connected to port ${PORT}`)
+})
 
 // database connection
-const DB_URI = process.env.MONGODB_URI || 'mongodb://localhost/v11-bears-08';
+const DB_URI = process.env.MONGODB_URI || 'mongodb://localhost/v11-bears-08'
 mongoose.connect(
   DB_URI,
   { useNewUrlParser: true, useCreateIndex: true },
   () => {
-    console.info(`Connected to db via ${DB_URI}`);
+    console.info(`Connected to db via ${DB_URI}`)
   }
-);
+)
