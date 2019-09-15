856adf5fff302afe4db9af2e049efe25f9474b31
moniet <moniet.sawhney@gmail.com>
2019-09-15 11:03:42 +0530

    added type scale

--- client/src/Global.js
+++ client/src/Global.js
@@ -11,10 +11,53 @@ export default () => (
 
       body {
         font-family: 'Poppins', 'Helvetica', 'Arial', sans-serif;
+        font-weight: 400;
+        line-height: 1.45;
       }
 
       a {
         text-decoration: ${theme.colors.primary};
+      }
+
+      p {
+        margin-bottom: 1.25em;
+      }
+
+      h1,
+      h2,
+      h3,
+      h4,
+      h5 {
+        margin: 2.75rem 0 1rem;
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+        line-height: 1.15;
+      }
+
+      h1 {
+        margin-top: 0;
+        font-size: 3.052em;
+      }
+
+      h2 {
+        font-size: 2.441em;
+      }
+
+      h3 {
+        font-size: 1.953em;
+      }
+
+      h4 {
+        font-size: 1.563em;
+      }
+
+      h5 {
+        font-size: 1.25em;
+      }
+
+      small,
+      .text_small {
+        font-size: 0.8em;
       }
     `}
   />
