a138ec35bcc910aee38424cc795084b4534096e0
fletcher-greg <fletcher.web.dev.one@gmail.com>
2019-09-19 18:32:23 +0200

    rebased

---
+++ src/resources/components/message/Message.model.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema // extract Schema from mongoose.Schema
+
+const messageSchema = new Schema(
+  {
+    message: {
+      text: { type: String, required: true }
+    },
+
+    users: [
+      {
+        user: {
+          type: Schema.Types.ObjectId,
+          ref: 'User',
+          required: true
+        }
+      }
+    ],
+    sender: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    read: { type: Date }
+  },
+  {
+    timestamps: true
+  }
+)
+
+const Message = mongoose.model('Message', messageSchema)
+module.exports = Message
