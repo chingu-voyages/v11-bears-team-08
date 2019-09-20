6091271235523f083691cf8145344be0bd381915
moniet <moniet.sawhney@gmail.com>
2019-09-20 16:15:13 +0530

    initalised repo with previous components

---
+++ client/src/Global.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import { Global, css } from '@emotion/core'
+import { theme } from './utils/theme'
+
+export default () => (
+  <Global
+    styles={css`
+      html {
+        font-size: 16px;
+      }
+
+      * {
+        box-sizing: border-box;
+      }
+
+      ::-webkit-input-placeholder {
+        color: white;
+      }
+      ::-moz-placeholder {
+        color: white;
+      }
+      :-ms-input-placeholder {
+        color: white;
+      }
+      :-moz-placeholder {
+        color: white;
+      }
+
+      body {
+        font-family: 'Poppins', 'Helvetica', 'Arial', sans-serif;
+        font-weight: 400;
+        line-height: 1.45;
+      }
+
+      a {
+        text-decoration: ${theme.colors.primary};
+      }
+
+      p {
+        margin-bottom: 1.25em;
+      }
+
+      h1,
+      h2,
+      h3,
+      h4,
+      h5 {
+        margin: 2.75rem 0 1rem;
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+        line-height: 1.15;
+      }
+
+      h1 {
+        margin-top: 0;
+        font-size: 3.052em;
+      }
+
+      h2 {
+        font-size: 2.441em;
+      }
+
+      h3 {
+        font-size: 1.953em;
+      }
+
+      h4 {
+        font-size: 1.563em;
+      }
+
+      h5 {
+        font-size: 1.25em;
+      }
+
+      small,
+      .text_small {
+        font-size: 0.8em;
+      }
+    `}
+  />
+)
---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,80 @@
+import React, { useContext } from 'react'
+import styled from '@emotion/styled'
+import { mq } from '../../utils/responsiveUtils'
+import { BrowserRouter as Router, Link } from 'react-router-dom'
+import { theme } from '../../utils/theme'
+
+const Nav = styled.nav`
+  position: absolute;
+  display: flex;
+  justify-content: flex-end;
+  top: 0;
+  left: 0;
+  padding: 2rem;
+  width: 100vw;
+  height: 5vh;
+  max-width: 100vw;
+  z-index: 2000;
+`
+
+const List = styled.ul`
+  display: flex;
+
+  li {
+    margin-left: 2em;
+  }
+
+  a {
+    color: ${theme.colors.primary};
+    &:hover {
+      color: ${theme.colors.secondary};
+    }
+  }
+`
+
+const Settings = styled.div`
+  display: block;
+  width: 1.25em;
+  height: 1.25em;
+  cursor: pointer;
+
+  &:hover {
+    svg {
+      fill: ${theme.colors.secondary};
+    }
+  }
+`
+
+export default () => {
+  return (
+    <Router>
+      <Nav>
+        <List>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/Login">Login</Link>
+          </li>
+          <li>
+            <Link to="/Register">Register</Link>
+          </li>
+          <li>
+            <Settings>
+              <svg
+                viewBox="0 0 36 26"
+                xmlns="http://www.w3.org/2000/svg"
+                fillRule="evenodd"
+                clipRule="evenodd"
+                strokeLinejoin="round"
+                strokeMiterlimit="2"
+              >
+                <path d="M8.887 2.716c0-.955.77-1.728 1.729-1.728h23.205c.955 0 1.729.767 1.729 1.728 0 .954-.769 1.728-1.729 1.728H10.616c-.954 0-1.729-.767-1.729-1.728zM2.469 4.937C1.105 4.937 0 3.832 0 2.469 0 1.105 1.105 0 2.469 0c1.363 0 2.469 1.105 2.469 2.469 0 1.363-1.106 2.468-2.469 2.468zm0 10.863C1.105 15.8 0 14.695 0 13.331c0-1.363 1.105-2.469 2.469-2.469 1.363 0 2.469 1.106 2.469 2.469 0 1.364-1.106 2.469-2.469 2.469zm0 9.875C1.105 25.675 0 24.57 0 23.206c0-1.363 1.105-2.469 2.469-2.469 1.363 0 2.469 1.106 2.469 2.469 0 1.364-1.106 2.469-2.469 2.469zm6.418-12.591c0-.954.77-1.728 1.729-1.728h23.205c.955 0 1.729.767 1.729 1.728 0 .955-.769 1.729-1.729 1.729H10.616c-.954 0-1.729-.768-1.729-1.729zm0 10.369c0-.954.77-1.728 1.729-1.728h23.205c.955 0 1.729.767 1.729 1.728 0 .955-.769 1.728-1.729 1.728H10.616c-.954 0-1.729-.767-1.729-1.728z" />
+              </svg>
+            </Settings>
+          </li>
+        </List>
+      </Nav>
+    </Router>
+  )
+}
