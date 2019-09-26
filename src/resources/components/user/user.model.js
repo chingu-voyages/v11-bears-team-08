f7db50f83193289479e1d223998f1e3a17077401
SebastianLF <slf.pro@gmail.com>
2019-09-27 00:03:11 +0200

    chnge label for 'type' discriminator key

--- src/resources/components/user/user.model.js
+++ src/resources/components/user/user.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
 
 const baseOptions = {
-  discriminatorKey: 'type',
+  discriminatorKey: '__type',
   collection: 'users'
 }
 
