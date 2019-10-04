50b3976dee909588a4fb4a61d0ce7886ec139d08
fletcher-greg <fletcher.web.dev.one@gmail.com>
2019-10-04 23:06:31 +0200

    added front-end api functions
    Co-authored-by: lunify <51797974+lunify@users.noreply.github.com>

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,8 +2,42 @@ import axios from 'axios'
 
 let api = axios.create({ baseURL: '/api' })
 
-function exampleAPICall() {
-  return api.get('/').then((res) => res.data)
+const authApi = {
+  async signup(formData) {
+    const { data } = await api.post('/auth/signup', formData)
+
+    if (!data.error) {
+      // !TODO grab token
+      // api() data.token
+    }
+
+    return data
+  },
+
+  async signin(formData) {
+    const { data } = await api.post('/auth/signin', formData)
+
+    if (!data.error) {
+      // !TODO grab token
+      // api() data.token
+    }
+
+    return data
+  },
+
+  async getCachedUser() {
+    try {
+      const { data } = await api.get('/auth/getCachedUser', {
+        withCredentials: true
+      })
+
+      return data
+    } catch (error) {
+      if (error.response.status === 401) {
+        // TODO repond with appropriate api
+      }
+    }
+  }
 }
 
-export { exampleAPICall }
+export { authApi }
