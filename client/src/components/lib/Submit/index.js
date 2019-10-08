a4aa6234894419f3755055c2dca773ae896d13fc
moniet <moniet.sawhney@gmail.com>
2019-10-08 23:29:17 +0530

    added cursor pointer to submit btn

--- client/src/components/lib/Submit/index.js
+++ client/src/components/lib/Submit/index.js
@@ -1,12 +1,22 @@
 import React from 'react'
-import styled from '@emotion/styled-base'
+import styled from '@emotion/styled'
 import { theme } from '../../../utils/theme'
 
 const Submit = styled.input`
   border: none;
   border-radius: 2px;
-  background: ${theme.colors.secondary};
-  padding: 0.25em;
+  background: ${theme.colors.dark};
+  padding: 0.5em 0.7em;
+  color: white;
+  font-size: 0.75em;
+  align-self: flex-start;
+   {
+    /* positioned to the left */
+  }
+  cursor: pointer;
+   {
+    /* Cuz duh! */
+  }
 `
 
 export default () => <Submit type="submit" value="SUBMIT" />
