e91b3513ce3558a8f8cab7f5bc61e1ddf1557ee5
SebastianLF <slf.pro@gmail.com>
2019-11-13 13:27:19 +0100

    implement async select for cities options

--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from '@emotion/styled'
 import { theme } from '../../utils/theme'
+import AsyncSelect from 'react-select/async'
+import { trainerApi } from '../../services/api'
 
 const Form = styled.div`
   position: relative;
@@ -30,13 +32,38 @@ const Input = styled.input`
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.3);
 `
 
-export default () => (
-  <Form role="form">
-    <Icon src={require('../../assets/img/search.svg')} alt="search button" />
-    <Input
+export default () => {
+  const [searchText, setSearchText] = useState('')
+  const [citiesOptions, setCitiesOptions] = useState([])
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const result = await trainerApi.getCities('par')
+  //     const formattedOptions = result.cities.map((city) => ({ value: city.id, label: `${city.name}, ${city.country}` }))
+  //     console.log(formattedOptions)
+  //     setCitiesOptions(formattedOptions)
+  //   }
+  //   fetchData()
+  // }, [searchText])
+  const promiseOptions = async (inputValue) => {
+    const cities = await trainerApi.getCities(inputValue)
+    const formattedCities = cities.cities.map((city) => ({
+      value: city.id,
+      label: city.name
+    }))
+    console.log(formattedCities)
+    return formattedCities
+  }
+
+  return (
+    <Form role="form">
+      <Icon src={require('../../assets/img/search.svg')} alt="search button" />
+      {/* <Input
       type="text"
       aria-label="search for coach by entering residence area name"
       placeholder="eg. Westminster"
-    />
-  </Form>
-)
+    /> */}
+      <AsyncSelect defaultOptions={true} loadOptions={promiseOptions} />
+    </Form>
+  )
+}
