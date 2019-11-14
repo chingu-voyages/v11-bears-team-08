ae1112654f660014f4299bde89739767e3830bc3
SebastianLF <slf.pro@gmail.com>
2019-11-14 12:27:19 +0100

    implement select input & display cities on  front

--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import styled from '@emotion/styled'
 import { theme } from '../../utils/theme'
-import AsyncSelect from 'react-select/async'
+import Select from 'react-select'
 import { trainerApi } from '../../services/api'
 
 const Form = styled.div`
@@ -10,60 +10,73 @@ const Form = styled.div`
   width: 100%;
 `
 
-const Icon = styled.img`
-  display: block;
-  position: absolute;
-  width: 32px;
-  height: auto;
-  left: 0;
-  top: 0;
-`
+// const Icon = styled.img`
+//   display: block;
+//   position: absolute;
+//   width: 32px;
+//   height: auto;
+//   left: 0;
+//   top: 0;
+// `
 
-const Input = styled.input`
-  position: relative;
-  padding: 1em;
-  width: 100%;
-  font-size: 1.25em;
-  letter-spacing: 2px;
-  background: ${theme.colors.dark};
-  border-radius: 20px;
-  color: white;
-  outline: none;
-  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.3);
-`
+// const Input = styled.input`
+//   position: relative;
+//   padding: 1em;
+//   width: 100%;
+//   font-size: 1.25em;
+//   letter-spacing: 2px;
+//   background: ${theme.colors.dark};
+//   border-radius: 20px;
+//   color: white;
+//   outline: none;
+//   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.3);
+// `
+
+const selectStyles = {
+  valueContainer: (styles) => ({ padding: '1em', border: 'none' })
+}
 
 export default () => {
   const [searchText, setSearchText] = useState('')
   const [citiesOptions, setCitiesOptions] = useState([])
+  const [selectedOptionId, setSelectedOptionId] = useState(null)
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const result = await trainerApi.getCities('par')
-  //     const formattedOptions = result.cities.map((city) => ({ value: city.id, label: `${city.name}, ${city.country}` }))
-  //     console.log(formattedOptions)
-  //     setCitiesOptions(formattedOptions)
-  //   }
-  //   fetchData()
-  // }, [searchText])
-  const promiseOptions = async (inputValue) => {
-    const cities = await trainerApi.getCities(inputValue)
-    const formattedCities = cities.cities.map((city) => ({
+  const formatCitiesOptions = (options) =>
+    options &&
+    options.map((city) => ({
       value: city.id,
-      label: city.name
+      label: `${city.name}, ${city.county ? city.county + ', ' : ''} ${
+        city.administrative
+      }, ${city.country} `
     }))
-    console.log(formattedCities)
-    return formattedCities
-  }
+
+  useEffect(() => {
+    const fetchCitiesOptions = async () => {
+      const citiesOptions = await trainerApi.getCities(searchText)
+      const formattedCitiesOptions = formatCitiesOptions(citiesOptions)
+      setCitiesOptions(formattedCitiesOptions)
+    }
+    fetchCitiesOptions()
+  }, [searchText])
 
   return (
     <Form role="form">
-      <Icon src={require('../../assets/img/search.svg')} alt="search button" />
-      {/* <Input
-      type="text"
-      aria-label="search for coach by entering residence area name"
-      placeholder="eg. Westminster"
-    /> */}
-      <AsyncSelect defaultOptions={true} loadOptions={promiseOptions} />
+      <Select
+        aria-label="search for coach by entering residence city name"
+        placeholder="Search for a coach by city. e.g. Paris"
+        options={citiesOptions}
+        inputValue={searchText}
+        onInputChange={(inputValue) => setSearchText(inputValue)}
+        onChange={(optionPicked) =>
+          optionPicked
+            ? setSelectedOptionId(optionPicked.value)
+            : setSelectedOptionId(null)
+        }
+        isClearable="true"
+        escapeClearsValue="true"
+        blurInputOnSelect
+        styles={selectStyles}
+      />
     </Form>
   )
 }
